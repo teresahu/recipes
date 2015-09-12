13bf2f2b2a5af1b29b064ee2d21db2cb96e93ca5
Teresa Hu <teresa@teresahu.net>
2015-09-12 18:27:32 -0400

    Created chefs and recipes associations and tests

--- test/models/chef_test.rb
+++ test/models/chef_test.rb
@@ -4,4 +4,60 @@ class ChefTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @chef = Chef.new(chefname: "Gordon Ramsey", email: "ramsey@hellskitchen.com")
+  end
+
+  test "chef should be valid" do
+    assert @chef.valid?
+  end
+
+  test "chefname should be present" do
+    @chef.chefname = " "
+    assert_not @chef.valid?
+  end
+
+  test "chefname should not be too long" do
+    @chef.chefname = "a" * 41
+    assert_not @chef.valid?
+  end
+
+  test "chefname should not be too short" do
+    @chef.chefname = "aaaa"
+    assert_not @chef.valid?
+  end
+
+  test "email should be present" do
+    @chef.email = " "
+    assert_not @chef.valid?
+  end
+
+  test "email should not be too long" do
+    @chef.email = "a" * 101 + "@hellskitchen.com"
+    assert_not @chef.valid?
+  end
+
+  test "email should be unique" do
+    dup_chef = @chef.dup
+    dup_chef.email = @chef.email.upcase
+    @chef.save
+    assert_not dup_chef.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w(user@eee.com R_TDD-DS@eee.hello.org user@example.com first.last@eem.au laura+joe@monk.cm)
+    valid_addresses.each do |a|
+      @chef.email = a
+      assert @chef.valid?, '#{a.inspect} should be valid.'
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w(user@example,com user_at_eee.org user.name@example. eee@i_am_.com foo@ee+aar.com)
+    invalid_addresses.each do |a|
+      @chef.email = a
+      assert_not @chef.valid?, "#{a.inspect} should be invalid."
+    end
+  end
 end
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -4,4 +4,64 @@ class RecipeTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @chef = Chef.new(chefname: "Bob", email: "bob@example.com")
+    @recipe = @chef.recipes.build(name: "Chicken Parm", summary: "This is the
+              best Chicken Parm recipe ever!", description: "Heat oil, add onions, add tomato sauce, add chicken, cook for 20 minutes.")
+  end
+
+  test "recipe should be valid" do
+    assert @recipe.valid?
+  end
+
+  test "chef_id should be present" do
+    @recipe.chef_id = nil
+    assert_not @recipe.valid?
+  end
+
+  test "name should be present" do
+    @recipe.name = " "
+    assert_not @recipe.valid?
+  end
+
+  test "name length should not be too long" do
+    @recipe.name = "a" * 101
+    assert_not @recipe.valid?
+  end
+
+  test "name length should not be too short" do
+    @recipe.name = "aaaa"
+    assert_not @recipe.valid?
+  end
+
+  test "summary should be present" do
+    @recipe.summary = " "
+    assert_not @recipe.valid?
+  end
+
+  test "summary length should not be too long" do
+    @recipe.summary = "a" * 151
+    assert_not @recipe.valid?
+  end
+
+  test "summary length should not be too short" do
+    @recipe.summary = "a" * 9
+    assert_not @recipe.valid?
+  end
+
+  test "description should be present" do
+    @recipe.description = " "
+    assert_not @recipe.valid?
+  end
+
+  test "description length should not be too long" do
+    @recipe.description = "a" * 501
+    assert_not @recipe.valid?
+  end
+
+  test "description length should not be too short" do
+    @recipe.description = "a" * 19
+    assert_not @recipe.valid?
+  end
+
 end
