203e8611bb9b5cbc8d90a3e5c635f933e013f752
Teresa Hu <teresa@teresahu.net>
2015-09-13 01:07:42 -0400

    Created new and edit forms and partials

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -5,4 +5,6 @@ class Chef < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 105 }, uniqueness: {
             case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+
+
 end
